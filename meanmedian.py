048807cef0889f113c486b318f10395f478b7ada
ChelseaT-hue <thompc2652@clarkstate.edu>
2026-03-01 19:30:09 -0500

    update mean and median

--- meanmedian.py
+++ meanmedian.py
@@ -4,3 +4,47 @@
 #This is a guided practice. You can follow the video or your instructor will go
 #over this in class
 # video url: https://youtu.be/VGrRdUunjXg
+
+theList = [0,0,0,0,0]
+for i in range(5):
+    theList[i] = int(input("Enter number {i = 1}:"))
+    for i in theList:
+        print(i)
+    theList.sort()
+    print("Small to large:")
+    for i in theList:
+        print(i)
+    print("Large to small:")
+    theList.sort(reverse=True)
+    for i in theList:
+        print(i)
+    total = 0
+    amount = len(theList)
+    for i in theList:
+        total += i
+        average = total / amount
+        print(f"the mean is {average} and the median is {theList[2]}")
+           
+
+theList = [0,0,0,0,0]
+for i in range(len(theList)):
+        theList[i] = int(input(f"Enter number {i + 1}:"))
+        for i in theList:
+            print(i)
+        theList.sort()
+        print("Small to large:")
+        for i in theList:
+            print(i)
+        print("Large to small:")
+        theList.sort(reverse=True)
+        for i in theList:
+            print(i)
+        total = 0
+        amount = len(theList)
+        for i in theList:
+            total += i
+            average = total / amount
+        
+print(f"the mean is {average} and the median is {theList[2]}")
+           
+
